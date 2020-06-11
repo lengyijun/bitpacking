1add2f0f199291e0a296bec5024859bc62ad7fdd
sjtu-lyj <maundercurfew@mail.sjtu.edu.cn>
2020-06-11 21:51:35 +0800

    remove sse3

--- src/bitpacker4x.rs
+++ src/bitpacker4x.rs
@@ -5,64 +5,6 @@ use crate::Available;
 
 const BLOCK_LEN: usize = 32 * 4;
 
-#[cfg(any(target_arch = "x86_64"))]
-mod sse3 {
-
-    use super::BLOCK_LEN;
-    use crate::Available;
-
-    use std::arch::x86_64::__m128i as DataType;
-    use std::arch::x86_64::_mm_and_si128 as op_and;
-    use std::arch::x86_64::_mm_lddqu_si128 as load_unaligned;
-    use std::arch::x86_64::_mm_or_si128 as op_or;
-    use std::arch::x86_64::_mm_set1_epi32 as set1;
-    use std::arch::x86_64::_mm_slli_epi32 as left_shift_32;
-    use std::arch::x86_64::_mm_srli_epi32 as right_shift_32;
-    use std::arch::x86_64::_mm_storeu_si128 as store_unaligned;
-    use std::arch::x86_64::{
-        _mm_add_epi32, _mm_cvtsi128_si32, _mm_shuffle_epi32, _mm_slli_si128, _mm_srli_si128,
-        _mm_sub_epi32,
-    };
-
-    #[allow(non_snake_case)]
-    unsafe fn or_collapse_to_u32(accumulator: DataType) -> u32 {
-        let a__b__c__d_ = accumulator;
-        let ______a__b_ = _mm_srli_si128(a__b__c__d_, 8);
-        let a__b__ca_db = op_or(a__b__c__d_, ______a__b_);
-        let ___a__b__ca = _mm_srli_si128(a__b__ca_db, 4);
-        let _______cadb = op_or(a__b__ca_db, ___a__b__ca);
-        _mm_cvtsi128_si32(_______cadb) as u32
-    }
-
-    #[target_feature(enable = "sse3")]
-    unsafe fn compute_delta(curr: DataType, prev: DataType) -> DataType {
-        _mm_sub_epi32(
-            curr,
-            op_or(_mm_slli_si128(curr, 4), _mm_srli_si128(prev, 12)),
-        )
-    }
-
-    #[target_feature(enable = "sse3")]
-    #[allow(non_snake_case)]
-    unsafe fn integrate_delta(prev: DataType, delta: DataType) -> DataType {
-        let offset = _mm_shuffle_epi32(prev, 0xff);
-        let a__b__c__d_ = delta;
-        let ______a__b_ = _mm_slli_si128(delta, 8);
-        let a__b__ca_db = _mm_add_epi32(______a__b_, a__b__c__d_);
-        let ___a__b__ca = _mm_slli_si128(a__b__ca_db, 4);
-        let a_ab_abc_abcd: DataType = _mm_add_epi32(___a__b__ca, a__b__ca_db);
-        _mm_add_epi32(offset, a_ab_abc_abcd)
-    }
-
-    declare_bitpacker!(target_feature(enable = "sse3"));
-
-    impl Available for UnsafeBitPackerImpl {
-        fn available() -> bool {
-            is_x86_feature_detected!("sse3")
-        }
-    }
-}
-
 mod scalar {
 
     use super::BLOCK_LEN;
@@ -172,22 +114,12 @@ impl BitPacker for BitPacker4x {
 
     /// Returns the best available implementation for the current CPU.
     fn new() -> Self {
-        #[cfg(target_arch = "x86_64")]
-        {
-            if sse3::UnsafeBitPackerImpl::available() {
-                return BitPacker4x(InstructionSet::SSE3);
-            }
-        }
         BitPacker4x(InstructionSet::Scalar)
     }
 
     fn compress(&self, decompressed: &[u32], compressed: &mut [u8], num_bits: u8) -> usize {
         unsafe {
             match self.0 {
-                #[cfg(target_arch = "x86_64")]
-                InstructionSet::SSE3 => {
-                    sse3::UnsafeBitPackerImpl::compress(decompressed, compressed, num_bits)
-                }
                 InstructionSet::Scalar => {
                     scalar::UnsafeBitPackerImpl::compress(decompressed, compressed, num_bits)
                 }
@@ -204,13 +136,6 @@ impl BitPacker for BitPacker4x {
     ) -> usize {
         unsafe {
             match self.0 {
-                #[cfg(target_arch = "x86_64")]
-                InstructionSet::SSE3 => sse3::UnsafeBitPackerImpl::compress_sorted(
-                    initial,
-                    decompressed,
-                    compressed,
-                    num_bits,
-                ),
                 InstructionSet::Scalar => scalar::UnsafeBitPackerImpl::compress_sorted(
                     initial,
                     decompressed,
@@ -224,10 +149,6 @@ impl BitPacker for BitPacker4x {
     fn decompress(&self, compressed: &[u8], decompressed: &mut [u32], num_bits: u8) -> usize {
         unsafe {
             match self.0 {
-                #[cfg(target_arch = "x86_64")]
-                InstructionSet::SSE3 => {
-                    sse3::UnsafeBitPackerImpl::decompress(compressed, decompressed, num_bits)
-                }
                 InstructionSet::Scalar => {
                     scalar::UnsafeBitPackerImpl::decompress(compressed, decompressed, num_bits)
                 }
@@ -244,13 +165,6 @@ impl BitPacker for BitPacker4x {
     ) -> usize {
         unsafe {
             match self.0 {
-                #[cfg(target_arch = "x86_64")]
-                InstructionSet::SSE3 => sse3::UnsafeBitPackerImpl::decompress_sorted(
-                    initial,
-                    compressed,
-                    decompressed,
-                    num_bits,
-                ),
                 InstructionSet::Scalar => scalar::UnsafeBitPackerImpl::decompress_sorted(
                     initial,
                     compressed,
@@ -265,7 +179,6 @@ impl BitPacker for BitPacker4x {
         unsafe {
             match self.0 {
                 #[cfg(target_arch = "x86_64")]
-                InstructionSet::SSE3 => sse3::UnsafeBitPackerImpl::num_bits(decompressed),
                 InstructionSet::Scalar => scalar::UnsafeBitPackerImpl::num_bits(decompressed),
             }
         }
@@ -274,10 +187,6 @@ impl BitPacker for BitPacker4x {
     fn num_bits_sorted(&self, initial: u32, decompressed: &[u32]) -> u8 {
         unsafe {
             match self.0 {
-                #[cfg(target_arch = "x86_64")]
-                InstructionSet::SSE3 => {
-                    sse3::UnsafeBitPackerImpl::num_bits_sorted(initial, decompressed)
-                }
                 InstructionSet::Scalar => {
                     scalar::UnsafeBitPackerImpl::num_bits_sorted(initial, decompressed)
                 }
@@ -290,7 +199,7 @@ impl BitPacker for BitPacker4x {
 #[cfg(test)]
 mod tests {
     use super::BLOCK_LEN;
-    use super::{scalar, sse3};
+    use super::{scalar};
     use crate::tests::test_util_compatible;
     use crate::Available;
     use crate::{BitPacker, BitPacker4x};
